297d93983602c46a256926fedeae171d405aaf02
shafnashakeer <shafnasm20@gmail.com>
2024-07-27 14:43:05 +0530

    start creating

---
+++ models/users.js
@@ -0,0 +1,22 @@
+const Mongoose=required("mongoose")
+const userSchema=Mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true
+        },
+        phone:String,
+        email:{
+            type:String,
+            required:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    }
+)
+
+//convert schema to model//
+var userModel=Mongoose.model("users",userSchema)
+module.exports=userModel
